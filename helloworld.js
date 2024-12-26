bafc566b9be077818e2d64b4c69a13042ceeac1f
AbishekSathiyan <abidinesh2213.gmail.com>
2024-12-26 12:07:39 +0530

    Learned Operators

--- helloworld.js
+++ helloworld.js
@@ -22,12 +22,62 @@ function a()
     console.log(b)
 }
 a()
-*/
 
-const a=4 // global Scope variable  const keyword  not change value
-// var a=22  error a is already defined because a is a const keyword
-const s=10
+
+const a=4 / global Scope variable  const keyword  not change value
+var a=22  /error a is already defined because a is a const keyword const s=10
 console.log(a)
 console.log(s)
-//console.log(b) // error b is not defined because b is a let keyword local variable of block
-/* console.log(s) /* s is not defined because s is a var keyword local variable of function a() */
+   console.log(b) // error b is not defined because b is a let keyword local variable of block
+ console.log(s) /* s is not defined because s is a var keyword local variable of function a() 
+ console.log(typeof a)
+ console.log(typeof s)
+
+
+ Operators
+ Arithmetic Operators
+
+ a=7
+ b=9
+ console.log(a<=b)
+
+ var a=10
+ var b=++a
+ console.log(a)
+ console.log(b)
+
+ str1="EMC"
+ str2="emc"
+ str1==str2?console.log("Equal"):console.log("Not Equal")
+ raining=0
+ raining?console.log("Take an umbrella"):console.log("Sun Day")
+
+
+const name="Abi"
+var age=22
+var fav_color="Blue"
+rainy=0
+
+console.log("My Name is "+name)
+console.log("My Age is "+age)
+console.log("My Favourite Color is "+fav_color)
+console.log(rainy?"Its Rainy Take an umbrella":"No Rain,Sun Day")
+
+
+width=5
+height=8
+area=width*height
+console.log("Area of Rectangle is "+area)
+*/
+
+var my_age=22
+var frnd_age=20
+var tot_age=my_age+frnd_age
+console.log(is_older=my_age>frnd_age?"I am Older":"I am Younger")
+console.log("Total Age is "+tot_age)
+
+var favnum=4
+var doublefavnum=favnum*2
+console.log("Double of My Favourite Number is "+doublefavnum)
+
+console.log("My Age is:"+my_age,"My Friends Age is:"+frnd_age,is_older +" than my friend"+" My Favourite Number is:"+favnum,"Double of My Favourite Number is:"+doublefavnum)
